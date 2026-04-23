4a011c538add54aeb8364b616029385920498f30
ayishaom <ayisha14om@gmail.com>
2026-04-23 14:27:44 +0200

    feat: implement create and get session endpoints

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -1,8 +1,22 @@
+const Session = require("../models/Session");
 
+const createSession = async (req, res) => {
+  try {
+    const session = await Session.create(req.body);
+    res.status(201).json(session);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-const createSession = async (req, res) => {};
-
-const getSessions = async (req, res) => {};
+const getSessions = async (req, res) => {
+  try {
+    const sessions = await Session.find();
+    res.json(sessions);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const updateSession = async (req, res) => {};
 
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const sessionRoutes = require("./routes/sessionRoutes");
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/sessions", sessionRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "StudySprint API is running" });
